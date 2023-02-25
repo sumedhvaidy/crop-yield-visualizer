fad77e14c202ac0886c2f68ed240d70b532a2bd1
Sumedh Vaidyanathan <sumedh@pulsepoint.org>
2023-02-25 13:32:54 -0600

    Input file and submit

--- crop-yield-visualization/src/App.js
+++ crop-yield-visualization/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileInputChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle submitting the selected file here
+    console.log('Selected file:', selectedFile);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Import a File</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="fileInput" className="fileInputButton">
+          Choose File
+        </label>
+        <input
+          id="fileInput"
+          type="file"
+          accept=".csv,.xlsx,.pdf" // Limit file types to only allow CSV, XLSX, and PDF files
+          onChange={handleFileInputChange}
+        />
+        {selectedFile && <p>Selected file: {selectedFile.name}</p>}
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
