28a14f8e8ff835c22feadb27d0968234ca28e889
faddock <usasuyash@gmail.com>
2023-02-25 23:36:52 -0600

    added farm data and toggles; updated styles

--- crop-yield-visualization/src/App.js
+++ crop-yield-visualization/src/App.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 import Chance from 'chance';
 import './App.css';
 import farm1 from "./data/farm1";
+import farm2 from "./data/farm2";
+import farm3 from "./data/farm3";
+
 import MyImage from './dirt.PNG';
 const chance = new Chance();
 const sampleData = chance.n(() => {
@@ -15,6 +18,7 @@ const sampleData = chance.n(() => {
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [hoveredSquare, setHoveredSquare] = useState(null);
+  const [farmDataset, setFarmDataset] = useState(farm1);
 
   const handleSquareHover = (index) => {
     setHoveredSquare(index);
@@ -23,6 +27,17 @@ function App() {
   const handleFileInputChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
+
+  const handleFarmChange = (event) => {
+    if (event.target.name == "farm1") {
+      setFarmDataset(farm1)
+    }      
+    else if (event.target.name == "farm2") {
+      setFarmDataset(farm2)
+    } 
+    else setFarmDataset(farm3)
+
+  }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -39,7 +54,7 @@ function App() {
           'Content-Type': 'multipart/form-data; boundary=${form._boundary}',
         },
       });
-      console.log(response.data)
+      // console.log(response.data)
     } catch (error) {
       console.error('Error:', error);
     }
@@ -47,6 +62,12 @@ function App() {
 
   return (
     <div>
+      <div>
+        <button name="farm1" onClick={handleFarmChange}>Farm 1</button>
+        <button name="farm2" onClick={handleFarmChange}>Farm 2</button>
+        <button name="farm3" onClick={handleFarmChange}>Farm 3</button>
+        {/* <p>You clicked {farmDataset}</p> */}
+      </div>
       <div className="App">
         <h1>Import a File</h1>
         <form onSubmit={handleSubmit}>
@@ -74,15 +95,15 @@ function App() {
             className="grid-overlay-image"
           />
           <div className="grid-overlay">
-            {farm1.map((datum, index) => {
+            {{farmDataset}.farmDataset.map((datum, index) => {
               const keyMap = {
-                1: 'green',
+                1: 'purple',
                 2: 'blue',
-                3: 'orange',
-                4: 'purple',
-                5: 'white'
+                3: 'green',
+                4: 'yellow',
+                5: 'red'
               }
-              {console.log(datum.YIELDLBL)}
+              // {console.log(datum.YIELDLBL)}
               
               const desiredColor = keyMap[datum.YIELDLBL];
               return <div
